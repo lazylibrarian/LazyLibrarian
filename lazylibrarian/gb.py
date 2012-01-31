4a8acc2f489d697d0d4d3372c952de29c65b40aa
Mar2zz <marsjaco@gmail.com>
2012-01-31 21:47:35 +0100

    GBS more functional. Searchresults are quite exact.

--- lazylibrarian/gb.py
+++ lazylibrarian/gb.py
@@ -8,32 +8,53 @@ from lazylibrarian import logger, formatter, database
 
 class GoogleBooks:
 
-def find_book_name(name=None):
+    def __init__(self, name=None):
+        self.name = name
+        self.url = 'https://www.googleapis.com/books/v1/volumes?q='
 
-    url = 'https://www.googleapis.com/books/v1/volumes?q=' + urllib.quote_plus('intitle:' + self.name)
-    jsonresults = json.JSONDecoder().decode(urllib2.urlopen(url, timeout=30).read())
-    for item in jsonresults['items']:
-        try:
-            BookImgl = item['volumeInfo']['imageLinks']['thumbnail']
-        except KeyError:
-            logger.debug('No thumbnail found for: ' + self.name)
+    def find_book_name(self):
+
+        URL = self.url + urllib.quote_plus('intitle:' + self.name)
+        jsonresults = json.JSONDecoder().decode(urllib2.urlopen(URL, timeout=30).read())
+        print URL
+        booklist = []
+        #print jsonresults
+        for item in jsonresults['items']:
+            # try for author, if no author, skip result.
+            try:
+                item['volumeInfo']['authors'][0]
+            except KeyError:
+                logger.debug('No author found for %s, skipping this one ...' % item['volumeInfo']['title'])
+                break
+
+            booklist.append({
+                'authorname': item['volumeInfo']['authors'][0],
+                'bookid': item['volumeInfo']['id'],
+                'bookname': item['volumeInfo']['title'],
+                'booklink': item['volumeInfo']['canonicalVolumeLink'],
+                'bookrate': item['volumeInfo']['averageRating'],
+                'bookdesc': item['volumeInfo']['industryIdentifiers'][0]['identifier']
+                })
+
+            # need to find which items aren't common to be on GBS and set 'm in tries.
+            try:
+                booklist.append({'bookimgl': item['volumeInfo']['imageLinks']['thumbnail']})
+            except KeyError:
+                logger.debug('No thumbnail found for: ' + self.name)
+
+        return booklist
 
 
-def find_author_name(name=None):
-    url = 'https://www.googleapis.com/books/v1/volumes?q=' + urllib.quote_plus('inauthor:' + self.name)
-    jsonresults = json.JSONDecoder().decode(urllib.urlopen(url).read())
-    print jsonresults
-    for item in jsonresults['items']:
-        try:
-            BookImgl = item['volumeInfo']['imageLinks']['thumbnail']
-        except KeyError:
-            logger.debug('No thumbnail found for: ' + self.name)
 
-#            print item['volumeInfo']['title'], 'BookName'
-#            print item['volumeInfo']['language'], 'BookLang'
-#            print item['volumeInfo']['authors'][0:], 'AuthorNames'
-#            print item['volumeInfo']['pageCount'], 'BookPages'
-#            print item['volumeInfo']['imageLinks']
+    def find_author_name(name=None):
+        url = 'https://www.googleapis.com/books/v1/volumes?q=' + urllib.quote_plus('inauthor:' + name)
+        jsonresults = json.JSONDecoder().decode(urllib.urlopen(url).read())
+        print jsonresults
+        for item in jsonresults['items']:
+            try:
+                BookImgl = item['volumeInfo']['imageLinks']['thumbnail']
+            except KeyError:
+                logger.debug('No thumbnail found for: ' + self.name)
 
 
 
