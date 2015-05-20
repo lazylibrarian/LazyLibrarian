f3212b7c585dabfa6eecf3fa60a6d0c6584fce1c
daktak <daktak@gmail.com>
2015-05-20 15:45:10 +1000

    fixed nzbget for version 14

--- lazylibrarian/searchnzb.py
+++ lazylibrarian/searchnzb.py
@@ -4,6 +4,7 @@ from xml.etree import ElementTree
 from xml.etree.ElementTree import Element, SubElement
 
 import lazylibrarian
+import request
 
 from lazylibrarian import logger, database, formatter, providers, nzbget, sabnzbd, SimpleCache, notifiers, searchmag, classes
 
@@ -149,6 +150,7 @@ def DownloadMethod(bookid=None, nzbprov=None, nzbtitle=None, nzburl=None):
         download = sabnzbd.SABnzbd(nzbtitle, nzburl)
 
     elif lazylibrarian.NZBGET_HOST and not lazylibrarian.NZB_DOWNLOADER_BLACKHOLE:
+        """
         try:
             req = urllib2.Request(nzburl)
             if lazylibrarian.PROXY_HOST:
@@ -160,12 +162,14 @@ def DownloadMethod(bookid=None, nzbprov=None, nzbtitle=None, nzburl=None):
             logger.warn('Error fetching nzb from url: ' + nzburl + ' %s' % e)
             nzbfile = False;
         if (nzbfile):
-
-            nzb = classes.NZBDataSearchResult()
-            nzb.extraInfo.append(nzbfile)
-            nzb.resultType = "nzbdata"
-            nzb.name = nzbtitle
-            download = nzbget.sendNZB(nzb)
+        """
+        headers = {'User-Agent': USER_AGENT}
+        data = request.request_content(url=nzburl, headers=headers)
+        nzb = classes.NZBDataSearchResult()
+        nzb.extraInfo.append(data)
+        nzb.name = nzbtitle
+        nzb.url = nzburl
+        download = nzbget.sendNZB(nzb)
 
     elif lazylibrarian.NZB_DOWNLOADER_BLACKHOLE:
 
