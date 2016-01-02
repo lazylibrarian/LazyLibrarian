17d292c8f1b0fcee9998eb6e04773e5d7b605c26
phil borman <phil.borman@gmail.com>
2016-01-02 16:55:05 +0100

    Fixed postprocess job restarting

--- lazylibrarian/postprocess.py
+++ lazylibrarian/postprocess.py
@@ -66,11 +66,13 @@ def schedule_processor(action='Start'):
         for job in lazylibrarian.SCHED.get_jobs():
             if "processDir" in str(job):
                 return  # return if already running, if not, start a new one
-        lazylibrarian.SCHED.add_interval_job(postprocess.processDir, minutes=int(lazylibrarian.SCAN_INTERVAL))
+        lazylibrarian.SCHED.add_interval_job(processDir, minutes=int(lazylibrarian.SCAN_INTERVAL))
+        logger.debug("Started postprocessing job")
     else:
         for job in lazylibrarian.SCHED.get_jobs():
             if "processDir" in str(job):
                 lazylibrarian.SCHED.unschedule_job(job)
+                logger.debug("Stopped postprocessing job")
 
 def processDir(force=False):
     # rename this thread
