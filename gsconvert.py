d28f878702ec370bc158c86a9ccce0e581099303
phil borman <phil.borman@gmail.com>
2016-02-17 12:27:48 +0100

    Use a private copy of gs if found

--- gsconvert.py
+++ gsconvert.py
@@ -7,24 +7,29 @@ import subprocess
 if len(sys.argv) != 3:
     print "Usage: gsconvert input.pdf output.jpg"
 else:
-    params = ["which", "gs"]
-    try:
-        GS = subprocess.check_output(params, stderr=subprocess.STDOUT).strip()        
-        if os.path.isfile(GS):
-            try:
-                jpeg = sys.argv[2]
-                pdf = sys.argv[1]
-                if '[' in pdf:
-                    pdf = pdf.split('[')[0]
-                params = [GS, "-sDEVICE=jpeg", "-dNOPAUSE", "-dBATCH", "-dSAFER", "-dFirstPage=1", "-dLastPage=1",
-                          "-sOutputFile=" + jpeg, pdf ]
-                res = subprocess.check_output(params, stderr=subprocess.STDOUT)
-                if not os.path.isfile(jpeg):
-                    print("Failed: %s" % res)
-            except subprocess.CalledProcessError as e:
-                print("Failed: %s" % e)
-        else:
-            print ("Cannot find gs, [%s] unable to continue" % GS)
-    except:
-        print ("Cannot find gs, unable to continue")    
+    GS = os.path.join(os.getcwd(), "gs")
+    if not os.path.isfile(GS):
+        params = ["which", "gs"]
+        try:
+            GS = subprocess.check_output(params, stderr=subprocess.STDOUT).strip()        
+        except:
+            print("Cannot find gs, unable to continue")
+    if os.path.isfile(GS):
+        try:
+            params = [GS, "--version"]
+            res = subprocess.check_output(params, stderr=subprocess.STDOUT)
+            print("Using gs [%s] version %s" % (GS, res))
+            jpeg = sys.argv[2]
+            pdf = sys.argv[1]
+            if '[' in pdf:
+                pdf = pdf.split('[')[0]
+            params = [GS, "-sDEVICE=jpeg", "-dNOPAUSE", "-dBATCH", "-dSAFER", "-dFirstPage=1", "-dLastPage=1",
+                      "-sOutputFile=%s" % jpeg, pdf ]
+            res = subprocess.check_output(params, stderr=subprocess.STDOUT)
+            if not os.path.isfile(jpeg):
+                print("Failed: %s" % res)
+        except subprocess.CalledProcessError as e:
+            print("Failed: %s" % e)
+    else:
+        print ("Cannot find gs, [%s] unable to continue" % GS)
 
