8b5f6c6cf4a4c5f4233ab341eb6a9b167df0e7fa
Phil Borman <phil.borman@gmail.com>
2016-05-07 22:33:33 +0200

    Added explanatory text

--- lazylibrarian/deluge.py
+++ lazylibrarian/deluge.py
@@ -1,11 +1,13 @@
-#  This file is part of Headphones.
+#  This file is part of LazyLibrarian.
+#  It is just used to talk JSON to the Deluge WebUI
+#  A separate library lib.deluge_client is used to talk to the Deluge daemon
 #
-#  Headphones is free software: you can redistribute it and/or modify
+#  Lazylibrarian is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  Headphones is distributed in the hope that it will be useful,
+#  Lazylibrarian is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
