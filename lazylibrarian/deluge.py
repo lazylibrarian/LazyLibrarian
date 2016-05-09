dc556dbe109cdb178b2acc1e69720dd633f8de70
Phil Borman <phil.borman@gmail.com>
2016-05-09 16:01:17 +0200

    Use LL lib.requests, not system version

--- lazylibrarian/deluge.py
+++ lazylibrarian/deluge.py
@@ -13,7 +13,7 @@
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with Headphones.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LazyLibrarian.  If not, see <http://www.gnu.org/licenses/>.
 
 # Parts of this file are a part of SickRage.
 # Author: Mr_Orange <mr_orange@hotmail.it>
@@ -21,18 +21,6 @@
 # Adapted for Headphones by <noamgit@gmail.com>
 # URL: https://github.com/noam09
 #
-# SickRage is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# SickRage is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with SickRage. If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
 
@@ -44,7 +32,7 @@ import re
 import os
 import json
 import lazylibrarian
-import requests
+import lib.requests as requests
 from base64 import b64encode
 import traceback
 
