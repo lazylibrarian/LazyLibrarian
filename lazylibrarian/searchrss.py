c0ba1b3972e8074969d7088f8365225c695d3e03
Phil Borman <phil.borman@gmail.com>
2016-06-21 08:43:27 +0200

    Set maxsize to zero to disable size checking

--- lazylibrarian/searchrss.py
+++ lazylibrarian/searchrss.py
@@ -122,7 +122,8 @@ def processResultList(resultlist, author, title, book):
                 tor_size_temp = 1000
             tor_size = str(round(float(tor_size_temp) / 1048576, 2)) + ' MB'
 
-            if tor_size > formatter.check_int(lazylibrarian.REJECT_MAXSIZE, 0):
+            maxsize = formatter.check_int(lazylibrarian.REJECT_MAXSIZE, 0)
+            if maxsize and tor_size > maxsize:
                 rejected = True
                 logger.debug("Rejecting %s, too large" % torTitle)
 
