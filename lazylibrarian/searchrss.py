e676995f910a31b4a0ab3d134e7b2f90981502ed
Phil Borman <phil.borman@gmail.com>
2016-06-30 15:34:14 +0200

    Removed colon typo

--- lazylibrarian/searchrss.py
+++ lazylibrarian/searchrss.py
@@ -15,7 +15,7 @@ from lazylibrarian.searchnzb import NZBDownloadMethod
 
 def cron_search_rss_book():
     threading.currentThread().name = "CRON-SEARCHRSS"
-    search_rss_book():
+    search_rss_book()
 
 def search_rss_book(books=None, reset=False):
     threadname = threading.currentThread().name
