ff6513131508aff876f7d7be47cfb83094b01768
Phil Borman <phil.borman@gmail.com>
2017-02-11 12:37:30 +0100

    Code tidying

--- lazylibrarian/api.py
+++ lazylibrarian/api.py
@@ -823,7 +823,7 @@ class Api(object):
         dbentry = myDB.match('SELECT %sID from %ss WHERE %sID=%s' % (table, table, table, itemid))
         if dbentry:
             myDB.action('UPDATE %ss SET %sImg="%s" WHERE %sID=%s' %
-                        (table, table, 'cache' + os.sep + self.id + '.jpg', table, itemid))
+                        (table, table, 'cache' + os.sep + itemid + '.jpg', table, itemid))
         else:
             self.data = "%sID %s not found" % (table, itemid)
 
