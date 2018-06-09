f708cd4768ede7c044cb0ac0277f4a3dae5070cb
Phil Borman <phil.borman@gmail.com>
2018-06-09 10:01:49 +0200

    Extra system info

--- lazylibrarian/common.py
+++ lazylibrarian/common.py
@@ -736,46 +736,44 @@ def logHeader():
     try:
         from lib.unrar import rarfile
         version = rarfile.unrarlib.RARGetDllVersion()
-        header += "unrar library DLL version %s\n" % version
+        header += "unrar: DLL version %s\n" % version
     except Exception as e:
-        header += "unrar library missing: %s\n" % str(e)
+        header += "unrar: missing: %s\n" % str(e)
 
-    ssl_report = False
-    if ssl_report:
-        header += "openssl: %s\n" % getattr(ssl, 'OPENSSL_VERSION', None)
-        try:
-            # pyOpenSSL 0.14 and above use cryptography for OpenSSL bindings. The _x509
-            # attribute is only present on those versions.
-            # noinspection PyUnresolvedReferences
-            import OpenSSL
-            # noinspection PyUnresolvedReferences
-            from OpenSSL.crypto import X509
-            x509 = X509()
-            if getattr(x509, "_x509", None) is None:
-                header += "pyOpenSSL: module missing required functionality. Try upgrading to v0.14 or newer. You have "
-            header += "pyOpenSSL: %s\n" % getattr(OpenSSL, '__version__', None)
-            from OpenSSL import SSL
-        except ImportError:
-            header += "pyOpenSSL: module missing\n"
+    header += "openssl: %s\n" % getattr(ssl, 'OPENSSL_VERSION', None)
+    try:
+        # pyOpenSSL 0.14 and above use cryptography for OpenSSL bindings. The _x509
+        # attribute is only present on those versions.
+        # noinspection PyUnresolvedReferences
+        import OpenSSL
+        # noinspection PyUnresolvedReferences
+        from OpenSSL.crypto import X509
+        x509 = X509()
+        if getattr(x509, "_x509", None) is None:
+            header += "pyOpenSSL: module missing required functionality. Try upgrading to v0.14 or newer. You have "
+        header += "pyOpenSSL: %s\n" % getattr(OpenSSL, '__version__', None)
+        from OpenSSL import SSL
+    except ImportError:
+        header += "pyOpenSSL: module missing\n"
 
-        try:
-            import OpenSSL.SSL
-        except (ImportError, AttributeError) as e:
-            header += 'OpenSSL missing module/attribute: %s\n' % e
+    try:
+        import OpenSSL.SSL
+    except (ImportError, AttributeError) as e:
+        header += 'OpenSSL missing module/attribute: %s\n' % e
 
-        try:
-            # get_extension_for_class method added in `cryptography==1.1`; not available in older versions
-            # but need cryptography >= 1.3.4 for access from pyopenssl >= 0.14
-            # noinspection PyUnresolvedReferences
-            import cryptography
-            # noinspection PyUnresolvedReferences
-            from cryptography.x509.extensions import Extensions
-            if getattr(Extensions, "get_extension_for_class", None) is None:
-                header += "cryptography: module missing required functionality."
-                header += " Try upgrading to v1.3.4 or newer. You have "
-            header += "cryptography: %s\n" % getattr(cryptography, '__version__', None)
-        except ImportError:
-            header += "cryptography: module missing\n"
+    try:
+        # get_extension_for_class method added in `cryptography==1.1`; not available in older versions
+        # but need cryptography >= 1.3.4 for access from pyopenssl >= 0.14
+        # noinspection PyUnresolvedReferences
+        import cryptography
+        # noinspection PyUnresolvedReferences
+        from cryptography.x509.extensions import Extensions
+        if getattr(Extensions, "get_extension_for_class", None) is None:
+            header += "cryptography: module missing required functionality."
+            header += " Try upgrading to v1.3.4 or newer. You have "
+        header += "cryptography: %s\n" % getattr(cryptography, '__version__', None)
+    except ImportError:
+        header += "cryptography: module missing\n"
 
     try:
         import magic
@@ -791,9 +789,9 @@ def logHeader():
             ver = magic.libmagic._name
         except AttributeError:
             ver = 'missing'
-        header += "magic library %s\n" % ver
+        header += "magic: %s\n" % ver
     else:
-        header += "magic module missing\n"
+        header += "magic: missing\n"
 
     return header
 
