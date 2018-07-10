2c9bb2dbff294847c4b415ffb043d2c8f5ee09c4
Phil Borman <phil.borman@gmail.com>
2018-07-10 14:35:35 +0200

    Fix nzbget progress reporting

--- lazylibrarian/rssfeed.py
+++ lazylibrarian/rssfeed.py
@@ -88,5 +88,5 @@ def genFeed(ftype, limit=10, user=0, baseurl=''):
         language="en-US",
         lastBuildDate=datetime.datetime.now(),
         items=items)
-    logger.debug("Returning %s %s%s" % (len(items), ftype, plural(len(items))))
+    # logger.debug("Returning %s %s%s" % (len(items), ftype, plural(len(items))))
     return feed.rss()
